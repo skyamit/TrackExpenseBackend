6481495f296310fc5a659ad9bac6a3b96045a779
Amit Kumar <amit7646014@gmail.com>
2025-03-27 11:42:15 +0530

    Updated code for cors

--- index.js
+++ index.js
@@ -98,8 +98,8 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 const corsOpts = {
-  origin: frontendUrl,
-  credentials: true,
+  origin: "*",
+  // credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
 };
